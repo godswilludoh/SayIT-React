7028c82d0d42dd3a8439ad6ced482114da94333a
Godswill Udoh <godswilludoh@gmail.com>
2022-10-14 22:19:59 +0100

    worked on folder structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div className='App'>SayIT</div>;
+	return <div className='App'>SayIT Application</div>;
 }
 
 export default App;
