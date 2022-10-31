28faa02a599af3b9534c609e82d11823e8e38529
Godswill Udoh <godswilludoh@gmail.com>
2022-10-31 01:27:28 +0100

    worked on the index.html and nav

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,44 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 // import { HashLink } from 'react-router-hash-link';
+import { headerLinks } from '../../data/headerLinks';
 import './Navbar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-
 const Navbar = () => {
+	const [show, setShow] = useState(false);
+
+	function mobileNavbar() {
+		setShow(!show);
+	}
 	return (
 		<nav>
 			<section className='logoLogoHeading'>
-				<div className='sayITLogo'>
-					<img
-						src='/assets/SAYIT.jpeg'
-						height='80px'
-						width='100px'
-						alt='logo of the SayIT platform'
-					/>
-				</div>
+				<Link to='/'>
+					<div className='sayITLogo'>
+						<img
+							src='/assets/SAYIT.jpeg'
+							height='80px'
+							width='100px'
+							alt='logo of the SayIT platform'
+						/>
+					</div>
+				</Link>
+
 				<h1 className='logoHeading'>
 					Say<span className='IT'>IT</span>
 				</h1>
 			</section>
-			<label htmlFor='click' className='menu_button'>
-				{/* <i className='fas fa-bars' /> */}
-				<FontAwesomeIcon id='menuBar' icon = {faBars}/>
-			</label>
-			{/* <input type='checkbox' id='click' /> */}
+
 			<ul className='navLinkWrapper'>
-				<Link to='/'>HOME</Link>
-				<Link to='#whyUs'>WHY US</Link>
-				<Link to='/report'>REPORT</Link>
-				<Link to='/blog'>BLOG</Link>
-				<Link id='active' to='login'>
-					SIGN IN
-				</Link>
+				{headerLinks.map(({ id, link, text }) => {
+					return (
+						<li key={id}>
+							<NavLink
+								className={(navData) => (navData.isActive ? 'active' : '')}
+								to={link}
+							>
+								{text}
+							</NavLink>
+						</li>
+					);
+				})}
 			</ul>
+			<div className='navLinkWrapper'>
+				<button>
+					<Link id='active' to='/login'>
+						SIGN IN
+					</Link>
+				</button>
+			</div>
+			<div className='menu_button' onClick={mobileNavbar}>
+				<div className={show ? 'hide' : 'start'} />
+				<div className={show ? 'hide' : 'mid'} />
+				<div className={show ? 'hide' : 'end'} />
+				
+			</div>
 		</nav>
 	);
 };
 
 export default Navbar;
-
