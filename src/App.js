25d169a5c67f781eb811e0006374372e7ed33a82
TheoCathy <theocathyne@gmail.com>
2022-11-01 23:20:15 +0100

    upated the admin dashboard UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/nav/Navbar';
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/landingPage/LandingPage';
+import LandingPage from './pages/LandingPage/LandingPage';
 import Report from './pages/report/Report';
 import CreateAccount from './pages/createAccount/CreateAccount';
 import Blog from './pages/blog/Blog';
--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -3,54 +3,53 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './Blog.css'
+// import './Navbar.css'
 import Footer from "../../components/footerLandingPage/FooterLandingPage";
-// import Navbar from "../../components/nav/Navbar";
 import Blogsearch  from './Blogsearch';
 
-// Navbar for only the blog page
+
+// NAVBAR FOR JUST DE BLOG PAGE
 export const BlogNavBar = () => {
-  return (
-		<nav>
-			<section className='logoLogoHeading'>
-				<div className='sayITLogo'>
-					<img
-						src='/assets/SAYIT.jpeg'
-						height='80px'
-						width='100px'
-						alt='logo of the SayIT platform'
-					/>
-				</div>
-				<h1 className='logoHeading'>
-					Say<span className='IT'>IT</span>
-				</h1>
-			</section>
-			<label htmlFor='click' className='menu_button'>
-				{/* <i className='fas fa-bars' /> */}
-				<FontAwesomeIcon id='menuBar' icon = {faBars}/>
-			</label>
-			{/* <input type='checkbox' id='click' /> */}
-			<ul className='navLinkWrapper'>
-				<Link to='/'>HOME</Link>
-				<Link to='/whyUs'>WHY US</Link>
-				<Link to='/report'>REPORT</Link>
-				
-				<Link id='active' to='#'>
-					DASHBOARD
-				</Link>
-			</ul>
-		</nav>
-
-  )
-}
-
-
+	return (
+		  <nav>
+			  <section className='logoLogoHeading'>
+				  <div className='sayITLogo'>
+					  <img
+						  src='/assets/SAYIT.jpeg'
+						  height='80px'
+						  width='100px'
+						  alt='logo of the SayIT platform'
+					  />
+				  </div>
+				  <h1 className='logoHeading'>
+					  Say<span className='IT'>IT</span>
+				  </h1>
+			  </section>
+			  <label htmlFor='click' className='menu_button'>
+				  {/* <i className='fas fa-bars' /> */}
+				  <FontAwesomeIcon id='menuBar' icon = {faBars}/>
+			  </label>
+			  {/* <input type='checkbox' id='click' /> */}
+			  <ul className='navLinkWrapper'>
+				  <Link to='/'>HOME</Link>
+				  <Link to='/whyUs'>WHY US</Link>
+				  <Link to='/report'>REPORT</Link>
+				  
+				  <Link id='active' to='#'>
+					  DASHBOARD
+				  </Link>
+			  </ul>
+		  </nav>
+  
+	)
+  }
 // Rendering compontes here for the blog
 const Blog = () => {
 	return(
    <React.Fragment>
 
 
-		<BlogNavBar />
+		{/* <BlogNavBar /> */}
 		<Blogsearch/>
 		
 
