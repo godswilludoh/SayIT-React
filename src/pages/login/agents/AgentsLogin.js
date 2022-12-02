4a711c9c1497b0b6cbb2070f33e4db164bc9cb53
Adenigba <lameda789@gmail.com>
2022-12-02 20:14:57 +0100

    fixed the eye icon overlapping stuff and added littlt changes

--- src/pages/login/agents/AgentsLogin.js
+++ src/pages/login/agents/AgentsLogin.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React , { useState } from "react";
 import "./AgentsLogin.css";
 import styles from "./button.module.css";
 import Navbar from "../../../components/nav/Navbar";
+import { FaRegEye } from 'react-icons/fa';
+
+
 
 const AgentsLogin = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+
+
   return (
     <div className="agentAndAdminContainer">
       <Navbar />
@@ -19,7 +31,7 @@ const AgentsLogin = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
                     height={24}
-                    style={{ fill: "rgb(137, 134, 133)" }}
+                    style={{ fill: "rgb(0,0,0)" }}
                   >
                     <path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path>
                   </svg>
@@ -39,18 +51,22 @@ const AgentsLogin = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
                     height={24}
-                    style={{ fill: "rgb(137, 134, 133)" }}
+                    style={{ fill: "rgb(0,0,0)" }}
                   >
                     <path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path>
                   </svg>
                 </span>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   id="agentpassword"
                   placeholder="Password"
                   required
                 />
+                <span className="passwordEyeIcon">
+                  <FaRegEye onClick={togglePassword}/>
+                </span>
+                
               </div>
             </form>
             <button className="logInButton">LOGIN</button>
