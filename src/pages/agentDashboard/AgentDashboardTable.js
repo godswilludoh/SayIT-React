89f937525165c365fc203c126c95c855b21b29a6
Adenigba <lameda789@gmail.com>
2022-12-04 18:04:25 +0100

    finished UI touches to the agentdashboard section

--- src/pages/agentDashboard/AgentDashboardTable.js
+++ src/pages/agentDashboard/AgentDashboardTable.js
@@ -1,62 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import { FaPrint, FaTrash } from "react-icons/fa";
 import style from "../agentDashboard/AgentDashBoardTable.module.css";
+import AgentDashBoardModal from "./AgentDashBoardModal";
+
 
 const AgentDashboardTable = () => {
+  const [show, setShow] = useState(false);
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>S/N</th>
-          <th>Report ID</th>
-          <th>View Report</th>
-          <th>Status</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>001</td>
-          <td>
-            <button className={style.clickToSeeReport}>
-              click to view more
-            </button>
-          </td>
-          <td>
-            <select className={style.agentTableReportSelect}>
-              <option value="select" selected>
-                Select
-              </option>
-              <option value="view">In View</option>
-              <option value="completed">Completed</option>
-            </select>
-          </td>
-          <td>
-            <div className={style.delete_Print}>
-              <span className={style.deleteIcon}>
-                <FaPrint />
-              </span>
-              <span className={style.printIcon}>
-                <FaTrash />
-              </span>
-            </div>
-          </td>
-        </tr>
-        {/* <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>slim Joe</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>mai lafiya</td>
-        </tr> */}
-      </tbody>
-    </Table>
+    <>
+      <Table>
+        <thead>
+          <tr>
+            <th>S/N</th>
+            <th>Report ID</th>
+            <th>View Report</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>001</td>
+            <td>
+              <button 
+              onClick={() => setShow(true)}
+              className={style.clickToSeeReport}>
+                click to view more
+              </button>
+            </td>
+            <td>
+              <select className={style.agentTableReportSelect}>
+                <option value="select" selected>
+                  Select
+                </option>
+                <option value="view">In View</option>
+                <option value="completed">Completed</option>
+              </select>
+            </td>
+            <td>
+              <div className={style.delete_Print}>
+                <span className={style.deleteIcon}>
+                  <FaPrint />
+                </span>
+                <span className={style.printIcon}>
+                  <FaTrash />
+                </span>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>002</td>
+            <td>
+              <button 
+              onClick={() => setShow(true)}
+              className={style.clickToSeeReport}>
+                click to view more
+              </button>
+            </td>
+            <td>
+              <select className={style.agentTableReportSelect}>
+                <option value="select" selected>
+                  Select
+                </option>
+                <option value="view">In View</option>
+                <option value="completed">Completed</option>
+              </select>
+            </td>
+            <td>
+              <div className={style.delete_Print}>
+                <span className={style.deleteIcon}>
+                  <FaPrint />
+                </span>
+                <span className={style.printIcon}>
+                  <FaTrash />
+                </span>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>003</td>
+            <td>
+              <button 
+              onClick={() => setShow(true)}
+              className={style.clickToSeeReport}>
+                click to view more
+              </button>
+            </td>
+            <td>
+              <select className={style.agentTableReportSelect}>
+                <option value="select" selected>
+                  Select
+                </option>
+                <option value="view">In View</option>
+                <option value="completed">Completed</option>
+              </select>
+            </td>
+            <td>
+              <div className={style.delete_Print}>
+                <span className={style.deleteIcon}>
+                  <FaPrint />
+                </span>
+                <span className={style.printIcon}>
+                  <FaTrash />
+                </span>
+              </div>
+            </td>
+          </tr>
+          
+        </tbody>
+      </Table>
+    
+      <AgentDashBoardModal onClose={() => setShow(false)} show={show} />
+    </>
+
   );
 };
 
