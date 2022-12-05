fdbfa37dea7099b4667ddc71014ebdcc4d2c29d5
Godswill Udoh <godswilludoh@gmail.com>
2022-12-05 01:38:45 +0100

    worked on protected routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Layout from './utility/Layout';
 import LandingPage from './pages/landingPage/LandingPage';
 import { ContantUs } from './pages/landingPage/ContantUs';
 import Blog from './pages/blog/Blog';
@@ -22,35 +23,46 @@ import { EditProfile } from './pages/users/userdashboardPages/editProfile/EditPr
 import UserForgotPassword from './pages/login/userforgotpassword/UserForgotPassword';
 import { AdminProfile } from './pages/AdminDashboard/AdminProfile';
 import AgentProfilePage from './pages/agentProfilePage/AgentProfilePage';
+import Missing from './utility/Missing';
+import RequireAuth from './utility/RequireAuth';
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route index element={<LandingPage />} />
-				<Route path='report' element={<ReportForm />} />
-				<Route path='signup' element={<SignupAcc />} />
-				<Route path='blog' element={<Blog />} />
-				<Route path='contact' element={<ContantUs />} />
-				<Route path='/login' element={<UsersLogin />} />
-				<Route path='admin' element={<AdminLogin />} />
-				<Route path='agents' element={<AgentsLogin />} />
-				<Route path='users' element={<UserDashboard />} />
-				<Route path='agentDashboard' element={<AgentDashBoard />} />
-				<Route path='adminDashboard' element={<Admindash />} />
-				<Route
-					path='agentNotificationPage'
-					element={<AgentNotificationPage />}
-				/>
-				<Route path='agentProfilePage' element={<AgentProfilePage />} />
-				<Route path='adminUsers' element={<AdminUsers />} />
-				<Route path='adminReport' element={<AdminReport />} />
-				<Route path='adminAgents' element={<AdminAgents />} />
-				<Route path='makereport' element={<MakeReportForm />} />
-				<Route path='editprofile' element={<EditProfile />} />
-				<Route path='userforgotpassword' element={<UserForgotPassword />} />
-				<Route path='adminProfile' element={<AdminProfile />} />
-				<Route path='userforgotpassword' element={<UserForgotPassword />} />
+				<Route path='/' element={<Layout />}>
+					<Route index element={<LandingPage />} />
+					<Route path='report' element={<ReportForm />} />
+					<Route path='signup' element={<SignupAcc />} />
+					<Route path='blog' element={<Blog />} />
+					<Route path='contact' element={<ContantUs />} />
+					<Route path='/login' element={<UsersLogin />} />
+					<Route path='admin' element={<AdminLogin />} />
+					<Route path='agents' element={<AgentsLogin />} />
+
+					{/* Protected routes */}
+					<Route element={<RequireAuth />}>
+						<Route path='users' element={<UserDashboard />} />
+						<Route path='agentDashboard' element={<AgentDashBoard />} />
+						<Route path='adminDashboard' element={<Admindash />} />
+						<Route
+							path='agentNotificationPage'
+							element={<AgentNotificationPage />}
+						/>
+						<Route path='agentProfilePage' element={<AgentProfilePage />} />
+						<Route path='adminUsers' element={<AdminUsers />} />
+						<Route path='adminReport' element={<AdminReport />} />
+						<Route path='adminAgents' element={<AdminAgents />} />
+						<Route path='makereport' element={<MakeReportForm />} />
+						<Route path='editprofile' element={<EditProfile />} />
+						<Route path='userforgotpassword' element={<UserForgotPassword />} />
+						<Route path='adminProfile' element={<AdminProfile />} />
+						<Route path='userforgotpassword' element={<UserForgotPassword />} />
+					</Route>
+
+					{/* catch all */}
+					<Route path='*' element={<Missing />} />
+				</Route>
 			</Routes>
 			<ToastContainer />
 		</>
